3abfe07678debc22c19bd15b9d84a48a8e35c368
Your Username <your.email@example.com>
2025-08-17 12:15:44 +0530

    sixth commith

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://militaryassetmanagement-system-backend.onrender.com/api' || 'http://localhost:5000/api',
+  baseURL: 'https://militaryassetmanagement-system-backend.onrender.com/api',
 });
 
 // const BASE = 'https://military-a432.onrender.com'; 
 
-// const api = axios.create({
-//    baseURL: BASE ? `${BASE}/api` : '/api',
-//   withCredentials: true
-// })
 
 
 // Add token automatically
